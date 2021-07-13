9dc42d907f4e26da42f3b98f67d91a56e4d9c053
KTH <shuttlecock0@naver.com>
2021-07-13 11:32:52 +0900

    paymentService url 변경

--- bookdelivery/order/src/main/java/bookdelivery/external/PaymentService.java
+++ bookdelivery/order/src/main/java/bookdelivery/external/PaymentService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="payment", url="http://localhost:8084", fallback = PaymentServiceFallback.class)
+@FeignClient(name="payment", url="http://payment:8080", fallback = PaymentServiceFallback.class)
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")
